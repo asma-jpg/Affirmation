36d810baf7636a8fa3eae880fef3f73dd55fca5f
Asma <asma20rejeb@gmail.com>
2021-11-02 17:45:13 +0000

    second commit

--- app/src/main/java/com/asma/affirmation/adapter/ItemAdapter.kt
+++ app/src/main/java/com/asma/affirmation/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.asma.affirmation.R
@@ -15,8 +16,10 @@ class ItemAdapter(
     private val dataset: List<Affirmation>
     )
     : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -29,9 +32,11 @@ class ItemAdapter(
     /**
      * Replace the contents of a view (invoked by the layout manager)
      */
+
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount(): Int {
